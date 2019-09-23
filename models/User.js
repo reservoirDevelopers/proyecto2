ffd10329954ab6adf1d574918a34fdea3f827c1f
“Natalia <natgarea@gmail.com>
2019-09-23 12:03:57 +0200

    user model

--- models/User.js
+++ models/User.js
@@ -2,8 +2,26 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const userSchema = new Schema({
+  email: String,
   username: String,
-  password: String
+  password: String,
+  city: String,
+  country: String,
+  gender: { type: String, enum: [ 'female' , 'male', 'non-binary', 'prefer not to say' ]},
+  status: {
+    type: String,
+    enum: ["Pending confirmation", "Active"],
+    default: "Pending confirmation"
+  },
+  confirmationCode: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  image: String,
+  friends: { type: Array },
+
+
 }, {
   timestamps: {
     createdAt: 'created_at',
