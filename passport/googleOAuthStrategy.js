46914625dd2bc1ff75af1bc2dab6db91aa9cae97
“Natalia <natgarea@gmail.com>
2019-09-26 12:03:53 +0200

    require pass

--- passport/googleOAuthStrategy.js
+++ passport/googleOAuthStrategy.js
@@ -1,6 +1,7 @@
 const passport      = require('passport');
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User          = require('../models/User');
+require('dotenv').config();
 
 passport.use(
   new GoogleStrategy({
